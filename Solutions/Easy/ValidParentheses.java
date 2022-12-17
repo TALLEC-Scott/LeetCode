001f3d883125fb9893f5ab633f75f65a8ab9f9b8
scott.tallec <scott.tallec@epita.fr>
2022-12-17 13:45:06 +0100

    feat : add ValidParenteses.java

---
+++ Solutions/Easy/ValidParentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static boolean valid(char c1, char c2)
+    {
+        if (c1 == '(' && c2 == ')')
+            return true;
+        if (c1 == '[' && c2 == ']')
+            return true;
+        if (c1 == '{' && c2 == '}')
+            return true;
+        return false;
+    }
+    public boolean isValid(String s) {
+        HashSet<Character> close_bracket = new HashSet<>(List.of(')', ']', '}'));
+        char c;
+        var stk = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            c = s.charAt(i);
+            if (!close_bracket.contains(c))
+                stk.add(c);
+            else if (stk.empty() || !Solution.valid(stk.pop(), c))
+                return false;
+        }
+        return stk.empty();
+    }
+}
