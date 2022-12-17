4c53787e25208d4e610c6ed66c5b3437cebc7170
scott.tallec <scott.tallec@epita.fr>
2022-12-17 13:48:45 +0100

    feat - add OddEvenLinkedList.java

---
+++ Solutions/Medium/OddEvenLinkedList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+        var odd = head;
+        if (head == null)
+            return head;
+        var even_start = head.next;
+        var even = even_start;
+        if (even == null)
+            return head;
+        while (true) {
+            odd.next = even.next;
+            if(odd.next == null)
+                break;
+            odd = odd.next;
+            even.next = odd.next;
+            if (even.next == null)
+                break;
+            even = even.next;
+            }
+        odd.next = even_start;
+        return head;
+    }
+}
