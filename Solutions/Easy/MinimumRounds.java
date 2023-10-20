a3b79212a4f796ed483acb1ff634c41a680718e3
scott.tallec <scott.tallec@epita.fr>
2023-10-20 17:39:44 +0200

    feat: add Min Rounds solution

---
+++ Solutions/Easy/MinimumRounds.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class MinimumRounds {
+    public int minimumRounds(int[] tasks) {
+
+        int curr = tasks[0];
+        int count;
+        int i = 1;
+        while(curr == tasks[i])
+        {
+            
+            i++;
+
+        }
+
+        return -1;
+
+    }
+}
